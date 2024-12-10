77ad12409a2013d4c0e617eeb4460a3b384c2b04
sumin <cleonn7878@gmail.com>
2024-12-11 06:08:44 +0900

    Feat: 여러 기기에서 스트림가능한 코드 추가

---
+++ stream_multi.py
@@ -0,0 +1,36 @@
+from flask import Flask, Response
+import cv2
+import threading
+
+app = Flask(__name__)
+
+cap = cv2.VideoCapture(0)
+lock = threading.Lock()  # 동기화 Lock
+
+def generate_frames():
+    global cap
+    while True:
+        with lock:  # 동기화
+            success, frame = cap.read()
+        if not success:
+            break
+        else:
+
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video')
+def video_feed():
+
+    return Response(generate_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    try:
+        app.run(host='0.0.0.0', port=5000)
+    finally:
+
+        cap.release()
